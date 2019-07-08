c5bcc9e7e0b5935e01f6285edf4bb6f314440f25
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2019-07-08 17:38:52 -0500

    effects exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -13,11 +13,21 @@ $(document).ready(function() {
         $('ul').each(function(){
             $(this).children().last().toggleClass( 'highlighter')
         })
+        // $("li:last-child").toggleClass('highlighter');
     });
+
     $('h3').on('click', function(){
+        $(this).next().children().slideToggle(100);
         $(this).next().children().css('font-weight', 'bold');
     });
     $('li').on('click', function(){
        $(this).parent().children().first().css('color', 'blue')
     });
+    $('.close').click(function(){
+        $(this).parent().hide()
+    });
+    $('h2').hide().delay(8000).fadeIn(1000).delay(8000).fadeOut(1000);
+
+
+
 });
