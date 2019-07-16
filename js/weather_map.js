b8555dfec5f7fae9b9b5829af05397fa0bf62faa
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2019-07-16 00:25:10 -0500

    weather map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,41 +1,76 @@
-$(document).ready(function(){
-    $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + weatherBox +  "/29.4241,-98.4936").done(function(source) {
+"use strict";
+
+$(document).ready(function() {
+    $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + weatherBox + "/29.4241,-98.4936").done(function (source) {
         console.log(source);
         $('#insertData').html(buildHtml(source));
 
         function buildHtml(data) {
             var weather = "";
             // data.forEach(function(data){
-                weather += '<div class="text-dark col-4">';
-                weather += '<img src="img/">';
-                weather += '<div>' + 'high ' + data.daily.data[0].apparentTemperatureHigh + '/' + 'Low ' + data.daily.data[0].apparentTemperatureLow + '</div>';
-                weather += '<div>' + data.currently.summary + '</div>';
-                weather += '<div>' + "Current Temperature: " +  data.currently.temperature + '</div>';
-                weather += '<div>' + 'Wind: ' + data.currently.windSpeed + '</div>';
-                weather += '<div>' + 'humidity: ' + data.currently.humidity + '</div>';
-                weather += '</div>';
-                console.log(weather);
+            weather += '<div class="bg-info col-12 margin-auto">';
+            weather += '<h1>' + "weather for today" + '</h1>';
+            weather += '<h2>' + 'San Antonio' + '</h2>';
+            weather += '<h5 class="center">' + 'high ' + data.daily.data[0].apparentTemperatureHigh + '/' + 'Low ' + data.daily.data[0].apparentTemperatureLow + '</h5>';
+            weather += '<img class="center" src="img/">';
+            weather += '<div>' + data.currently.summary + '</div>';
+            weather += '<div>' + "Current Temperature: " + data.currently.temperature + '</div>';
+            weather += '<div>' + 'Wind: ' + data.currently.windSpeed + '</div>';
+            weather += '<div>' + 'humidity: ' + data.currently.humidity + '</div>';
+            weather += '</div>';
+            console.log(weather);
             // });
             return weather;
         }
 
-        $('#getCoords').on('click',function(e) {
-            e.preventDefault();
-            var lat = $('#lati').val();
-            var long = $('#long').val();
-            function reverseGeocode(coordinates, token) {
-                var baseUrl = 'https://api.mapbox.com';
-                var endPoint = '/geocoding/v5/mapbox.places/';
-                return fetch(baseUrl + endPoint + long + "," + lat + '.json' + "?" + 'access_token=' + token)
-                    .then(function (res) {
-                        return res.json(    )
-                .then(function(data) {
-                    return data.features[0].place_name;
-                    });
-                });
-            }
+        $('#insertData2').html(function () {
+            var weather = '';
+            weather += '<div class="col-12 bg-info center">';
+            weather += '<h1>' + 'weather for tomorrow' + '</h1>';
+            weather += '<h5 class=center>' + 'high ' + source.daily.data[1].apparentTemperatureHigh + '/Low ' + source.daily.data[1].apparentTemperatureLow + '</h5>';
+            weather += '<img src="img/">';
+            weather += '<div class="center">' + source.daily.data[1].summary + '</div>';
+            weather += '<div>' + 'humidity: ' + source.daily.data[1].humidity + '</div>';
+            weather += '<div>' + 'wind: ' + source.daily.data[1].windSpeed + '</div>';
+            return weather;
         });
 
+        $('#insertData3').html(function () {
+            var weather = '';
+            weather += '<div class="col-12 bg-info center">';
+            weather += '<h1>' + 'weather for after tomorrow' + '</h1>';
+            weather += '<h5 class=center>' + 'high ' + source.daily.data[2].apparentTemperatureHigh + '/Low ' + source.daily.data[1].apparentTemperatureLow + '</h5>';
+            weather += '<img src="img/">';
+            weather += '<div class="center">' + source.daily.data[2].summary + '</div>';
+            weather += '<div>' + 'humidity: ' + source.daily.data[2].humidity + '</div>';
+            weather += '<div>' + 'wind: ' + source.daily.data[2].windSpeed + '</div>';
+            return weather;
+        });
+
+
+        // $('#getCoords').on('click', function (e) {
+        //     var lat = $('#lati').val();
+        //     var long = $('#long').val();
+        //     var request = $.get('https://api.darksky.net/forecast/'+weatherBox+'/' + lat +',' + long);
+        //     console.log(request);
+        //     e.preventDefault();
+        //     var coords = [];
+        //     // $('#tomorrow').html(buildHtml(request));
+
+
+        //     function reverseGeocode(coordinates, token) {
+        //         var baseUrl = 'https://api.mapbox.com';
+        //         var endPoint = '/geocoding/v5/mapbox.places/';
+        //         return fetch(baseUrl + endPoint + long + "," + lat + '.json' + "?" + 'access_token=' + token)
+        //             .then(function (res) {
+        //                 return res.json()
+        //                     .then(function (data) {
+        //                         return data.features[0].place_name;
+        //                     });
+        //             });
+        //     }
+        // });
+        //
         mapboxgl.accessToken = mapboxtoken;
         var map = new mapboxgl.Map({
             container: 'map',
@@ -46,20 +81,21 @@ $(document).ready(function(){
         });
 
 
-        function reverseGeocode(coordinates, token) {
-            var baseUrl = 'https://api.mapbox.com';
-            var endPoint = '/geocoding/v5/mapbox.places/';
-            return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-                .then(function(res) {
-                    return res.json();
-                })
-                // to get all the data from the request, comment out the following three lines...
-                .then(function(data) {
-                    return data.features[0].place_name;
-                });
-        }
-    });
+        //     function reverseGeocode(coordinates, token) {
+        //         var baseUrl = 'https://api.mapbox.com';
+        //         var endPoint = '/geocoding/v5/mapbox.places/';
+        //         return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+        //             .then(function(res) {
+        //                 return res.json();
+        //             })
+        //             // to get all the data from the request, comment out the following three lines...
+        //             .then(function(data) {
+        //                 return data.features[0].place_name;
+        //             });
+        //     }
+        // });
 
         // var dateObject = new Date(data.currently.time * 1000);
         // console.log(dateObject);
+    });
 });
