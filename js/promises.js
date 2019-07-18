be1cf3f2fcdcbb47554c1775f11dccd23ca9f476
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-18 11:12:20 -0500

    promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,16 @@
-const wait = new Promise((resolve, reject) => {
+const wait = (input) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+                resolve('ding! promise is done!');
+        }, input)
+    });
+};
+const getUsernames = (name) => {
+    return fetch('https://api.github.com/users/:username/events', {headers: {'Authorization': 'token' + gitToken}})
+        .then(response => response.json())
+        // .then(users => users.map(user => user.login));
+};
+console.log(getUsernames());
 
-});
-
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
