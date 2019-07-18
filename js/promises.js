485e411525c7be4d48e4d77140370bb7a2dfc952
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-18 15:32:22 -0500

    promises

--- js/promises.js
+++ js/promises.js
@@ -5,20 +5,36 @@ const wait = (input) => {
         }, input)
     });
 };
-const getUsernames = () => {
-    return fetch('https://api.github.com/users', {'headers': {'Authorization': 'token ' + gitToken}})
+// const getUsernames = () => {
+//     return fetch('https://api.github.com/users', {'headers': {'Authorization': 'token ' + gitToken}})
+//         .then(response => response.json())
+//         .then(users => users.map(user => user.login));
+// };
+//
+// getUsernames().then((usernames) =>{
+//     for (let username of usernames) {
+//         // console.log(username);
+//         fetch(`https://api.github.com/users/${username}/events`, {'headers': {'Authorization': 'token ' + gitToken}})
+//             .then(response => response.json())
+//             .then(info => console.log(info[0].created_at))
+//     }
+// });
+
+const getLastPush = username => {
+    return fetch(`https://api.github.com/users/${username}/events`, {'headers': {'Authorization': 'token ' + gitToken}})
         .then(response => response.json())
-        .then(users => users.map(user => user.login));
+        .then(events => {
+            for (let event of events) {
+                if (event.type === 'PushEvent') {
+                    console.log(`${event.type} at ${event.created_at}`);
+                    break;
+                }
+            }
+        })
 };
 
-getUsernames().then((usernames) =>{
-    for (let username of usernames) {
-        // console.log(username);
-        fetch(`https://api.github.com/users/${username}/events`, {'headers': {'Authorization': 'token ' + gitToken}})
-            .then(response => response.json())
-            .then(info => console.log(info[0].created_at))
-    }
-});
+getLastPush('aaronsifuentes47');
+
 
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
