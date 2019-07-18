5d8a68fa7d869c4e5afbeadc6d2fb57760fb74c0
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-18 12:02:05 -0500

    promises exercises

--- js/promises.js
+++ js/promises.js
@@ -5,12 +5,21 @@ const wait = (input) => {
         }, input)
     });
 };
-const getUsernames = (name) => {
-    return fetch('https://api.github.com/users/:username/events', {headers: {'Authorization': 'token' + gitToken}})
+const getUsernames = () => {
+    return fetch('https://api.github.com/users', {'headers': {'Authorization': 'token ' + gitToken}})
         .then(response => response.json())
-        // .then(users => users.map(user => user.login));
+        .then(users => users.map(user => user.login));
 };
-console.log(getUsernames());
+
+getUsernames().then((usernames) =>{
+    for (let username of usernames) {
+        // console.log(username);
+        fetch(`https://api.github.com/users/${username}/events`, {'headers': {'Authorization': 'token ' + gitToken}})
+            .then(response => response.json())
+            .then(info => console.log(info[0].created_at))
+    }
+});
+
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
